920c3ff1f0866860767a0cb7344781d044a9f0cc
NeaPieroelie <nea.pieroelie@gmail.com>
2025-03-18 13:08:55 -0700

    worked on styling and added a public index file to link styling

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
